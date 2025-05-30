bed851a565a5b432543b6533457035e73d282f5e
Simfeeee <toxaslyadnev@gmail.com>
2025-05-30 14:40:12 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
-bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+
+bot = Bot(
+    token=TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 dp = Dispatcher()
