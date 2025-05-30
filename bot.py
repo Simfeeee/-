4bf8b6560a23d1374e0ae9b9f419467726028c90
Simfeeee <toxaslyadnev@gmail.com>
2025-05-30 15:01:00 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+import asyncio
 import os
+
+from scheduler import start_scheduler
 
 TOKEN = os.getenv("BOT_TOKEN")
 
@@ -10,3 +13,10 @@ bot = Bot(
     default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
 dp = Dispatcher()
+
+async def main():
+    start_scheduler(bot)  # Запуск планировщика
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
