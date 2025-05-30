db1fb706e93e850789dd0af73587dfda48e08106
Simfeeee <toxaslyadnev@gmail.com>
2025-05-30 17:03:59 +0300

    config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 
 RSS_FEEDS = ['https://lenta.ru/rss/news', 'https://www.kommersant.ru/RSS/news.xml', 'https://www.interfax.ru/rss.asp', 'https://iz.ru/rss', 'https://www.rbc.ru/static/rss/news.rss', 'https://www.gazeta.ru/export/rss/lenta.xml', 'https://meduza.io/rss/all', 'https://news.google.com/rss/search?q=россия', 'https://yandex.ru/news/rss']
 CHANNEL_ID = os.environ.get('CHANNEL_ID')
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME",
