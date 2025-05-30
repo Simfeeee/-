a4d635f2dedc321ff3552842b340c7bfaaf9f39b
Simfeeee <toxaslyadnev@gmail.com>
2025-05-30 14:28:40 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,18 +1,7 @@
-import asyncio
-import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
-from scheduler import start_scheduler
+import os
 
 TOKEN = os.getenv("BOT_TOKEN")
-
-bot = Bot(token=TOKEN, default=types.DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
-
-start_scheduler(bot)
-
-async def main():
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
